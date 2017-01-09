599bacf07e5746ce79012b61b64e9c8559cb34b0
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-01-09 17:25:03 -0600

    basic game loop works

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberWizard : MonoBehaviour {
+	public Text WizardSays;
+
+	int guesscount = 0;	
+	int max = 10000;
+	int min = 1;
+	int guess = 5000;
+	int max_guesses = 5;
+	// Use this for initialization
+
+
+
+
+
+	 void GuessAgain (){
+
+		if (guesscount > max_guesses) {
+		//	LevelManager.LoadLevel("Win");
+		//figure out why the above code doesn't work
+		SceneManager.LoadScene ("Win");
+			}
+
+
+		else {
+			guess = (max + min) / 2;
+			WizardSays.text = "Is your numberzzz: "+guess+"?";
+		   }
+		
+
+		}
+
+
+		
+	public void Start(){
+		GuessAgain ();
+
+	}
+
+	
+
+
+
+
+
+
+#region buttonhandlers
+public void GuessHigher(){
+		print("Up pressed");
+		min = guess;
+		GuessAgain();
+		guesscount++;
+
+
+
+}
+
+public void GuessLower(){
+
+		print("Down pressed");
+			max = guess;
+			GuessAgain();
+			guesscount++;
+}
+
+
+public void WizardWon(){
+// game goes to lose screen. 
+
+}
+
+
+#endregion
+
+}
